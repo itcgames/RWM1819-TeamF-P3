88659c003d5c00e5f2d34e7237c755b060e8a36f
John-OMeara <C00206417@itcarlow.ie>
2019-01-22 14:03:56 +0000

    Base functionality ready. Needs implentation

--- src/Interface.js
+++ src/Interface.js
@@ -1,5 +1,10 @@
 class Interface {
 
+  /**
+   * Creates the interface object
+   * @param {number} screenWidth Width of the game screen
+   * @param {number} screenHeight Height of the game screen
+   */
   constructor(screenWidth,screenHeight) {
 
     this.active = false;
@@ -26,8 +31,21 @@ class Interface {
       x: 0,
       y: this.defaultHeight,
     };
+
+    this.map = {
+      x: 50,
+      y: 250,
+      w: 250,
+      h: 150,
+    };
+
+    
   }
 
+  /**
+   * Triggers the pause menu. Starts it moving in the opposite direction.
+   * 
+   */
   trigger() {
     this.moving = true;
     this.active = false;
@@ -49,46 +67,81 @@ class Interface {
     }
   }
 
+  /**
+   * Updates the UI. Checks if it should be moving, handles it's active state
+   * functionality
+   */
   update() {
     if(this.moving) {
-
-      // Check if the UI is at the top of the screen
-      if(this.pos.y < this.defaultHeight){
-        console.log("HIT TOP");
-        this.pos.y = this.defaultHeight;
-        this.moving = false;
-
-        this.atTop = true;
-
-        /* resume gameplay */
-        this.trigger();
-      }
-
-      // Check if the UI is at the bottom
-      else if (this.pos.y > 0){
-        console.log("HIT BOTTOM");
-        this.pos.y = 0;
-        this.moving = false;
-
-        this.active = true;
-        
-        /* update menu logic */
-        this.trigger();
-      }
-
-      this.pos.y += this.scrollSpeed;
+      move();
     }
     else if (this.active) {
       // MENU LOGIC GOES HERE
     }
-
-    console.log(this.pos.y - this.defaultHeight);
   }
 
+  /**
+   * Moves the UI plane. Checks the position of the plane to determine the
+   * state of the UI.
+   */
+  move(){
+    // Check if the UI is at the top of the screen
+    if(this.pos.y < this.defaultHeight){
+      console.log("HIT TOP");
+      this.pos.y = this.defaultHeight;
+      this.moving = false;
+
+      this.atTop = true;
+
+      /* resume gameplay */
+      this.trigger();
+    }
+
+    // Check if the UI is at the bottom
+    else if (this.pos.y > 0){
+      console.log("HIT BOTTOM");
+      this.pos.y = 0;
+      this.moving = false;
+
+      this.active = true;
+      
+      /* update menu logic */
+      this.trigger();
+    }
+
+    this.pos.y += this.scrollSpeed;
+  }
+
+  /**
+   * Renders the interface.
+   * Positions are relative to the position of the pause menu screen's position
+   * @param {ctx} ctx Context
+   */
   render(ctx) {
+    ctx.fillStyle = "#000000";
     ctx.fillRect(this.pos.x,this.pos.y,this.screen.w,this.screen.h);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(
+      this.map.x + this.pos.x,
+      this.map.y + this.pos.y,
+      this.map.w,
+      this.map.h
+    );
+
+    ctx.font = "60px comic sans"
+    ctx.fillText(
+      "Inventory", 
+      50 + this.pos.x, 
+      70 + this.pos.y);
   }
 
+  /**
+   * Called whenever player collects/spends an item so that the UI can
+   * reflect the current state
+   * @param {number} rupees Total rupee count
+   * @param {number} bombs Total bomb count
+   * @param {number} keys Total key count
+   */
   updateCounts(rupees, bombs, keys) {
     this.rupeeCount = rupees;
     this.bombCount = bombs;
